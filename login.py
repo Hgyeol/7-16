1234f0eaf176ada9edf3e6be20fd2a33a06bd940
Hgyeol <neunimchi626@gmail.com>
2024-07-16 16:52:26 +0900

    last

--- login.py
+++ login.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate('secret.json')
+cred = credentials.Certificate('.gitignore/secrets.json')
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
@@ -12,12 +12,21 @@ if not firebase_admin._apps:
 
 gId = db.reference('id').get()
 gPassword = db.reference('password').get()
-st.write(type(gId))
+#st.write(type(gId))
 st.title("로그인")
 
 id = st.text_input("아이디", "")
 password = st.text_input("비밀번호", "")
 
 if st.button("로그인"):
-    if id == gId & password == gPassword:
+    st.write(str(id))
+    st.write(str(gId))
+    if str(id) == str(gId) & str(password) == str(gPassword):
         st.switch_page("pages/Main.py")
+    else :
+        st.write("일치하지 않음")
+
+st.write(id)
+st.write(gId)
+st.write(password)
+st.write(gPassword)
